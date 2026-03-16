1f7fb5ea35d1a11c45a0b61f1a4a61f0f42e4b24
eca <116784250+sodiumKlorida@users.noreply.github.com>
2026-03-16 22:03:25 +0700

    week3

---
+++ week3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using week3;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        // AUTOMATA
+        materiAutomata.automataRun();
+    }
+}
---
+++ week3/materiAutomata.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week3
+{
+    internal class materiAutomata
+    {
+        // STATE (KEADAAN/PERISTIWA)
+        public enum State
+        {
+            START,
+            GAME,
+            PAUSE,
+            EXIT
+        }
+
+        public static void automataRun()
+        {
+            State state = State.START;
+
+            while (state != State.EXIT)
+            {
+                switch (state)
+                {
+                    case State.START:
+                        state = Start();
+                        break;
+
+                    case State.GAME:
+                        state = Game();
+                        break;
+
+                    case State.PAUSE:
+                        state = Pause();
+                        break;
+                }
+            }
+
+            Exit();
+        }
+
+        // STATE START
+        public static State Start()
+        {
+            Console.WriteLine("START SCREEN");
+            Console.WriteLine("Hint");
+            Console.WriteLine("- ENTER");
+            Console.WriteLine("- QUIT");
+            Console.Write("Enter Command : ");
+
+            string command = Console.ReadLine();
+
+            if (command == "ENTER")
+                return State.GAME;
+            else if (command == "QUIT")
+                return State.EXIT;
+            else
+                return State.START;
+        }
+
+        // STATE GAME
+        public static State Game()
+        {
+            Console.WriteLine("\nGAME SCREEN");
+            Console.WriteLine("Hint");
+            Console.WriteLine("- ESC");
+            Console.Write("Enter Command : ");
+
+            string command = Console.ReadLine();
+
+            if (command == "ESC")
+                return State.PAUSE;
+            else
+                return State.GAME;
+        }
+
+        // STATE PAUSE
+        public static State Pause()
+        {
+            Console.WriteLine("\nPAUSE SCREEN");
+            Console.WriteLine("Hint");
+            Console.WriteLine("- BACK");
+            Console.WriteLine("- HOME");
+            Console.WriteLine("- QUIT");
+            Console.Write("Enter Command : ");
+
+            string command = Console.ReadLine();
+
+            if (command == "BACK")
+                return State.GAME;
+            else if (command == "HOME")
+                return State.START;
+            else if (command == "QUIT")
+                return State.EXIT;
+            else
+                return State.PAUSE;
+        }
+
+        // EXIT SCREEN
+        public static void Exit()
+        {
+            Console.WriteLine("EXIT SCREEN");
+        }
+    }
+}
+
